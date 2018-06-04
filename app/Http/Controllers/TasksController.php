8c218ff4751fa789b8ec3849647e41883f360700
ayumu.otomo <aymotm1993@gmail.com>
2018-06-04 06:08:58 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,13 +42,19 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    { $this->validate($request, [
+    {$this->validate($request, [
+            'status' => 'required|max:10',   // add
             'content' => 'required|max:191',
         ]);
 
+
         $task = new Task;
+        $task->status = $request->status;    // add
         $task->content = $request->content;
         $task->save();
+
+
+        return redirect('/');
         
 }
     /**
@@ -89,15 +95,20 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    { $this->validate($request, [
+    {  $this->validate($request, [
+            'status' => 'required|max:10',   // add
             'content' => 'required|max:191',
         ]);
 
+
         $task = Task::find($id);
+        $task->status = $request->status;    // add
         $task->content = $request->content;
         $task->save();
 
-        return redirect('/');    }
+
+        return redirect('/');
+    }
 
     /**
      * Remove the specified resource from storage.
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
+
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
@@ -11,6 +15,5 @@
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
-<!-- Write content for each page here -->
 
 @endsection
