04067ffad7d54f3cdf146c3d13d91d37968928b1
ayumu.otomo <aymotm1993@gmail.com>
2018-06-13 07:35:26 +0000

    fireman

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -22,7 +22,13 @@
     
      <div class="row">
         <div class=col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-            {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
-            {!! Form::model($task,['route'=> ['tasks.destroy',$task->id],'method' => 'delete'])!!}
-            {!!Form::submit('削除',['class' => 'btn btn-danger']) !!}
-            {!! Form::closed() !!}
+            {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+    
+    
+            {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+                {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
